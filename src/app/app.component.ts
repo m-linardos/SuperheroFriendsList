f85d1c7b99f6eb08b6be45d3bd771ea16c477eee
Marianne Linardos <mlinardos.edu@gmail.com>
2019-09-16 00:24:48 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'superheroes-friend-list';
+  title = 'My Superhero Friends List';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FriendsComponent } from './friends/friends.component';
+import { SuperPowersComponent } from './super-powers/super-powers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendsComponent,
+    SuperPowersComponent
   ],
   imports: [
     BrowserModule
